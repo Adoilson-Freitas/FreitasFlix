19dc37155e42d128d17cf4350bd1505aab7cca77
Adoilson Freitas <adoilsonfreitas@gmail.com>
2020-07-27 18:15:09 -0300

    dia 1 imersao react

--- src/App.js
+++ src/App.js
@@ -1,26 +1,26 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import Menu from './components/Menu';
+import data from './data/dados_iniciais.json';
+import BannerMain from './components/BannerMain';
+import Carousel from './components/Carousel';
+import Footer from './components/Footer';
 
-function App() {
+
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Menu />
+
+      <BannerMain
+        videoTitle={data.categorias[0].videos[0].titulo}
+        url={data.categorias[0].videos[0].url}
+      />
+      <Carousel category={data.categorias[4]} />
+      <Carousel ignoreFirstVideo category={data.categorias[0]} />
+      <Carousel category={data.categorias[1]} />
+      <Carousel category={data.categorias[2]} />
+
+      <Footer />
     </div>
   );
 }
-
-export default App;
