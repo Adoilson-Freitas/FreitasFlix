5f6b9dbdef553293f6c7ead957ad5e593fb7731b
Adoilson Freitas <adoilsonfreitas@gmail.com>
2020-08-03 15:35:05 -0300

    link para server

--- src/config/index.js
+++ src/config/index.js
@@ -1,6 +1,6 @@
 const URL_BACKEND_TOP = window.location.hostname.includes('localhost')
   ? 'http://localhost:3333'
-  : 'https://devsoutinhoflix.herokuapp.com';
+  : 'https://english-flix.herokuapp.com/';
 
 export default {
   URL_BACKEND_TOP,
